a87ac7c18aff50bf5ddc6645de047910433c9787
vineet <13799022+vineet131@users.noreply.github.com>
2023-09-22 15:43:40 +0900

    AMRSW-537 Make navigation Noetic compliant by removing tf

--- fake_localization/static_odom_broadcaster.py
+++ fake_localization/static_odom_broadcaster.py
@@ -11,9 +11,19 @@
 
 import rospy
 
-import tf
+from tf2_ros.transform_broadcaster import TransformBroadcaster
+from transforms3d.euler import euler2quat
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Pose, Quaternion, Point
+from geometry_msgs.msg import (
+    Point,
+    Pose,
+    Quaternion,
+    Transform,
+    TransformStamped,
+    Vector3
+)
+from std_msgs.msg import Header
+
 
 
 def publishOdom():
@@ -22,23 +32,29 @@ def publishOdom():
     odom_frame_id = rospy.get_param("~odom_frame_id", "odom")
     publish_frequency = rospy.get_param("~publish_frequency", 10.0)
     pub = rospy.Publisher('odom', Odometry)
-    tf_pub = tf.TransformBroadcaster()
+    tf_pub = TransformBroadcaster()
 
     #TODO: static pose could be made configurable (cmd.line or parameters)
-    quat = tf.transformations.quaternion_from_euler(0, 0, 0)
+    quat = euler2quat(0, 0, 0)
+    quat = Quaternion(quat[1], quat[2], quat[3], quat[0])
 
     odom = Odometry()
     odom.header.frame_id = odom_frame_id
-    odom.pose.pose = Pose(Point(0, 0, 0), Quaternion(*quat))
+    odom.pose.pose = Pose(Point(0, 0, 0), quat)
 
     rospy.loginfo("Publishing static odometry from \"%s\" to \"%s\"", odom_frame_id, base_frame_id)
     r = rospy.Rate(publish_frequency)
     while not rospy.is_shutdown():
         odom.header.stamp = rospy.Time.now()
         pub.publish(odom)
-        tf_pub.sendTransform((0, 0, 0), quat,
-                             odom.header.stamp, base_frame_id, odom_frame_id)
+        t_stamped = TransformStamped(
+            Header(stamp=odom.header.stamp, frame_id=odom_frame_id),
+            base_frame_id,
+            Transform(Vector3(0, 0, 0), quat)
+        )
+        tf_pub.sendTransform(t_stamped)
         r.sleep()
+
 
 if __name__ == '__main__':
     try:
