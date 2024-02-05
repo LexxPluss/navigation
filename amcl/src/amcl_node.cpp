9f28c809ec54286f293ac894eea2813ce9f3aae0
yuanfanwang <wang.far.sail@gmail.com>
2024-02-05 17:49:58 +0900

    wip

--- amcl/src/amcl_node.cpp
+++ amcl/src/amcl_node.cpp
@@ -308,10 +308,16 @@ class AmclNode
     // odom correction
     bool odom_correction_;
     bool is_init_odom_correction_;
+    bool pause_odom_correction_;
     pf_vector_t pure_amcl_delta_sum_;
     pf_vector_t pure_odom_delta_sum_;
     double odom_amcl_x_thre_;
     double odom_amcl_z_thre_;
+    double odom_sum_dist_;
+    double paused_odom_a_ = 0.0;
+    double paused_odom_d_ = 0.0;
+    double paused_odom_a_thre_ = 0.5;
+    double paused_odom_d_thre_ = M_PI / 4.0;
     int odom_amcl_diff_count_thre_;
 
     void reconfigureCB(amcl::AMCLConfig &config, uint32_t level);
@@ -481,6 +487,8 @@ AmclNode::AmclNode() :
   private_nh_.param("odom_amcl_x_thre", odom_amcl_x_thre_, 0.2);
   private_nh_.param("odom_amcl_z_thre", odom_amcl_z_thre_, 0.01);
   private_nh_.param("odom_amcl_diff_count_thre", odom_amcl_diff_count_thre_, 5);
+  private_nh_.param("puased_odom_a_thre", paused_odom_a_thre_, M_PI/2.0);
+  private_nh_.param("puased_odom_d_thre", paused_odom_d_thre_, 1.0);
 
   // For diagnostics
   private_nh_.param("std_warn_level_x", std_warn_level_x_, 0.2);
@@ -552,6 +560,9 @@ AmclNode::AmclNode() :
   pure_odom_delta_sum_ = pf_vector_zero();
   diff_count_ = 0;
   is_init_odom_correction_ = false;
+  pause_odom_correction_ = true;
+  paused_odom_a_ = 0.0;
+  paused_odom_d_ = 0.0;
   diff_count_pub_ = nh_.advertise<std_msgs::Float32>("diff_count", 2, true);
   previous_amcl_pose_ = geometry_msgs::PoseWithCovarianceStamped();
   previous_base_link_pose_ = geometry_msgs::PoseWithCovarianceStamped();
@@ -1570,6 +1581,24 @@ AmclNode::laserReceived(const sensor_msgs::LaserScanConstPtr& laser_scan)
       {
         pose_pub_.publish(p);
         last_published_pose = p;
+      }
+      else if (pause_odom_correction_)
+      {
+        diff_count_ = 0;
+
+        paused_odom_a_ += std::fabs(delta.v[2]);
+        paused_odom_d_ += std::sqrt(delta.v[0]*delta.v[0] + delta.v[1]*delta.v[1]);
+        if (paused_odom_a_ > paused_odom_a_thre_ ||
+            paused_odom_d_ > paused_odom_d_thre_)
+        {
+          pause_odom_correction_ = false;
+          paused_odom_a_ = 0.0;
+          paused_odom_d_ = 0.0;
+        }
+        else{
+          pose_pub_.publish(p);
+          last_published_pose = p;
+        }
       }
       else
       {
