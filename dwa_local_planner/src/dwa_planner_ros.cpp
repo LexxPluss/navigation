9c85bc293181850ac1b82b99dc758f8f4fbaaabd
takayuki.wada <yakato.jun@gmail.com>
2024-02-16 15:10:21 +0900

    [AMRCS-153] fix typo.

--- dwa_local_planner/src/dwa_planner_ros.cpp
+++ dwa_local_planner/src/dwa_planner_ros.cpp
@@ -473,10 +473,10 @@ namespace dwa_local_planner {
         }
       }
       
-      double turn_turget_x = turn_target_pose.pose.position.x;
-      double turn_turget_y = turn_target_pose.pose.position.y;
+      double turn_target_x = turn_target_pose.pose.position.x;
+      double turn_target_y = turn_target_pose.pose.position.y;
 
-      double turn_turget_th = std::atan2(turn_turget_y - current_y, turn_turget_x - current_x);
+      double turn_target_th = std::atan2(turn_target_y - current_y, turn_target_x - current_x);
       double current_th = tf2::getYaw(this->current_pose_.pose.orientation);
 
 
@@ -489,14 +489,14 @@ namespace dwa_local_planner {
           return angle;
         };
 
-        double relative_cargo_angle_when_turned = normalize(this->current_cargo_angle_ - (turn_turget_th - current_th));
+        double relative_cargo_angle_when_turned = normalize(this->current_cargo_angle_ - (turn_target_th - current_th));
         double cargo_limit_angle_rad = this->cargo_limit_angle_deg_ * M_PI / 180.0;
 
         if (std::abs(relative_cargo_angle_when_turned) < cargo_limit_angle_rad) {
           if (relative_cargo_angle_when_turned > 0) {
-            turn_turget_th = normalize(current_th + this->current_cargo_angle_ - cargo_limit_angle_rad);
+            turn_target_th = normalize(current_th + this->current_cargo_angle_ - cargo_limit_angle_rad);
           } else {
-            turn_turget_th = normalize(current_th + this->current_cargo_angle_ + cargo_limit_angle_rad);
+            turn_target_th = normalize(current_th + this->current_cargo_angle_ + cargo_limit_angle_rad);
           }
         }
       }
@@ -506,14 +506,14 @@ namespace dwa_local_planner {
       bool was_rotate = startLatchedStopRotateController_.rotateToGoal(
         current_pose_,
         robot_vel,
-        turn_turget_th,
+        turn_target_th,
         cmd_vel,
         limits.getAccLimits(),
         dp_->getSimPeriod(),
         limits,
         boost::bind(&DWAPlanner::checkTrajectory, dp_, _1, _2, _3));
 
-      if (!was_rotate || std::abs(current_th - turn_turget_th) < (5.0 * M_PI / 180.0))
+      if (!was_rotate || std::abs(current_th - turn_target_th) < (5.0 * M_PI / 180.0))
       {
         this->rotate_to_goal_ = false;
       }
