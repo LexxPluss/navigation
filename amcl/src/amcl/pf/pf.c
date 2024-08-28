b63d0863f33f1899899c4fabefe86f17e17c6cb0
yuanfanwang <wang.far.sail@gmail.com>
2024-08-28 16:24:16 +0900

    functionalized updating weight process

--- amcl/src/amcl/pf/pf.c
+++ amcl/src/amcl/pf/pf.c
@@ -262,6 +262,32 @@ void pf_update_action(pf_t *pf, pf_action_model_fn_t action_fn, void *action_dat
   return;
 }
 
+void pf_update_weight(double total, pf_t *pf, pf_sample_set_t *set)
+{
+  pf_sample_t *sample;
+
+  // Normalize weights
+  double w_avg = 0.0;
+  for (int i = 0; i < set->sample_count; i++)
+  {
+    sample = set->samples + i;
+    w_avg += sample->weight;
+    sample->weight /= total;
+    set->n_effective += sample->weight*sample->weight;
+  }
+  // Update running averages of likelihood of samples (Prob Rob p258)
+  w_avg /= set->sample_count;
+  if (pf->w_slow == 0.0)
+    pf->w_slow = w_avg;
+  else
+    pf->w_slow += pf->alpha_slow * (w_avg - pf->w_slow);
+  if (pf->w_fast == 0.0)
+    pf->w_fast = w_avg;
+  else
+    pf->w_fast += pf->alpha_fast * (w_avg - pf->w_fast);
+  // printf("w_avg: %e slow: %e fast: %e\n", 
+          // w_avg, pf->w_slow, pf->w_fast);
+}
 
 #include <float.h>
 // Update the filter with some new sensor observation
@@ -283,30 +309,7 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
 
   if (!use_expansion_reset_)
   {
-    if (total > 0.0)
-    {
-      // Normalize weights
-      double w_avg = 0.0;
-      for (i = 0; i < set->sample_count; i++)
-      {
-        sample = set->samples + i;
-        w_avg += sample->weight;
-        sample->weight /= total;
-        set->n_effective += sample->weight*sample->weight;
-      }
-      // Update running averages of likelihood of samples (Prob Rob p258)
-      w_avg /= set->sample_count;
-      if (pf->w_slow == 0.0)
-        pf->w_slow = w_avg;
-      else
-        pf->w_slow += pf->alpha_slow * (w_avg - pf->w_slow);
-      if (pf->w_fast == 0.0)
-        pf->w_fast = w_avg;
-      else
-        pf->w_fast += pf->alpha_fast * (w_avg - pf->w_fast);
-      // printf("w_avg: %e slow: %e fast: %e\n", 
-              // w_avg, pf->w_slow, pf->w_fast);
-    }
+    if (total > 0.0) pf_update_weight(total, pf, set);
     else
     {
       // Handle zero total
@@ -317,11 +320,10 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       }
     }
   }
-   
-  // [https://ieeexplore.ieee.org/document/1389781]
-  // syo-kai probabilistic robotics P173
-  if (use_expansion_reset_)
+  else
   {
+    // [https://ieeexplore.ieee.org/document/1389781]
+    // syo-kai probabilistic robotics P173 {
     const double sigma[3] =
     {
       expansion_reset_x_sigma_,
@@ -334,28 +336,7 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       if (expansion_reset_likelihood_th_ > total) set->unlikely_count++;
       else set->unlikely_count--;
       if (set->unlikely_count < 0) set->unlikely_count = 0;
-
-      // Normalize weights
-      double w_avg = 0.0;
-      for (i = 0; i < set->sample_count; i++)
-      {
-        sample = set->samples + i;
-        w_avg += sample->weight;
-        sample->weight /= total;
-        set->n_effective += sample->weight*sample->weight;
-      }
-      // Update running averages of likelihood of samples (Prob Rob p258)
-      w_avg /= set->sample_count;
-      if(pf->w_slow == 0.0)
-        pf->w_slow = w_avg;
-      else
-        pf->w_slow += pf->alpha_slow * (w_avg - pf->w_slow);
-      if(pf->w_fast == 0.0)
-        pf->w_fast = w_avg;
-      else
-        pf->w_fast += pf->alpha_fast * (w_avg - pf->w_fast);
-      // printf("w_avg: %e slow: %e fast: %e\n", 
-              // w_avg, pf->w_slow, pf->w_fast);
+      pf_update_weight(total, pf, set);
     }
     else
     {
@@ -364,10 +345,7 @@ void pf_update_sensor(pf_t *pf, pf_sensor_model_fn_t sensor_fn, void *sensor_dat
       {
         sample = set->samples + i;
         sample->weight = 1.0 / set->sample_count;
-        for (int j = 0; j < 3; j++)
-        {
-          sample->pose.v[j] += pf_ran_gaussian(sigma[j]);
-        }
+        for (int j = 0; j < 3; j++) sample->pose.v[j] += pf_ran_gaussian(sigma[j]);
       }
       set->unlikely_count = 0;
     }
